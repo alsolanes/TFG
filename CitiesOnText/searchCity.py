30b32052172f8d817076113a429fe7aa898e0cdc
Aleix <al.solanes@gmail.com>
2015-05-10 22:38:23 +0200

    Integrated all the project to an IPython notebook

--- CitiesOnText/searchCity.py
+++ CitiesOnText/searchCity.py
@@ -19,6 +19,24 @@ def get_coords_city(nom):
     for city in a:
         print city
     return a
+def get_common_words():
+    from collections import Counter
+    import csv
+    db = get_db_ciutats()
+    paraules = []
+    total = db.ciutats.find().count()
+    a = db.ciutats.find()
+    i = 0
+    for ciutat in a:
+        i+=1
+        if i%1000 == 0: 
+            print i/total
+        paraula = ciutat['name'].replace(',','')
+        for w in paraula:
+            paraules.append(w.encode('utf-8'))
+    writer = csv.writer(open("common_words.csv",'w'))
+    writer.writerows(paraules)
+    print 'End'
 
 def get_city():
     print 'Beginning scan...'
@@ -33,4 +51,21 @@ def get_city():
         #print ciutat
         similars = get_coords_city(ciutat)
     print 'End Scan'
-get_city()
+
+
+##########################################################
+def get_db_news2():
+    from pymongo import MongoClient
+    client = MongoClient('localhost:27017')
+    db = client.noticies
+    return db.noticies.find_one()['descripcio']
+import nltk
+from nltk.corpus import cess_cat
+text = get_db_news2()
+opt1=cess_cat.word_tokenize(text)
+
+
+###########################################################333
+#get_city()
+#get_common_words()
+
